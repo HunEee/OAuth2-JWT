6aba78859bed7d165da1da43abbc7cf6ad923654
HunEee <asdf981001@naver.com>
2025-07-02 00:15:17 +0900

    프로젝트 생성

---
+++ src/test/java/com/spring_security/zAuthJwt/ZOAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_security.zAuthJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZOAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
