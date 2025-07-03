a5bbaa33e47b0e85ccc80a395cb8327633d2ec56
HunEee <asdf981001@naver.com>
2025-07-03 23:57:25 +0900

    커스텀 서비스 구현

--- src/main/java/com/spring_security/zAuthJwt/config/SecurityConfig.java
+++ src/main/java/com/spring_security/zAuthJwt/config/SecurityConfig.java
@@ -2,15 +2,23 @@ package com.spring_security.zAuthJwt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.spring_security.zAuthJwt.service.CustomOAuth2UserService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+        this.customOAuth2UserService = customOAuth2UserService;
+    }
+	
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,7 +33,10 @@ public class SecurityConfig {
         http.httpBasic((auth) -> auth.disable());
 
         //oauth2
-        http.oauth2Login(Customizer.withDefaults());
+        http.oauth2Login((oauth2) -> oauth2.userInfoEndpoint(
+        			(userInfoEndpointConfig) -> userInfoEndpointConfig.userService(customOAuth2UserService)
+        		)
+		);
 
         //경로별 인가 작업
         http.authorizeHttpRequests((auth) -> auth.requestMatchers("/").permitAll().anyRequest().authenticated());
---
+++ src/main/java/com/spring_security/zAuthJwt/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package com.spring_security.zAuthJwt.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private String role;
+    private String name;
+    private String username;
+}
